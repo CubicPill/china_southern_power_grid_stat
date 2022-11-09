2bf196de02f066945e90adbd5036427783d5add8
CubicPill <CubicPill@users.noreply.github.com>
2022-11-09 22:24:25 +0800

    change integration domain

---
+++ custom_components/china_southern_power_grid_stat/const.py
@@ -0,0 +1,3 @@
+"""Constants for the China Southern Power Grid Statistics integration."""
+
+DOMAIN = "china_southern_power_grid_stat"
--- custom_components/csg_stat/const.py
+++
@@ -1,3 +0,0 @@
-"""Constants for the China Southern Power Grid integration."""
-
-DOMAIN = "china_southern_power_grid"
